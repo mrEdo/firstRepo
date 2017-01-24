27b6ca903d53e5ad5fd7bccdf402cf32f2a5d28c
mrEdo <admin@Lab-2-Instructor.local>
2017-01-23 22:54:46 -0600

    got the basic js functionality working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,23 @@
 // My Custom Scripts
+
+var first = document.getElementById("firstName");
+var last = document.getElementById("lastName");
+var email = document.getElementById("emailAddress");
+var submit = document.getElementById("submitButton");
+var first = document.getElementById("firstName");
+var entries = document.getElementById("guestEntries");
+
+submit.addEventListener("click", submitGuest);
+
+function submitGuest(){
+
+
+	//alert( first.value + " " + last.value );
+	var newEntry = document.createElement("h1");
+	var newEntryText = document.createTextNode(first.value + " " + last.value + " | " + email.value);
+	newEntry.appendChild(newEntryText);
+	entries.appendChild(newEntry);
+	first.value = "";
+	last.value = "";
+	email.value = "";
+}
